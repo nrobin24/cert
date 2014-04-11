7e3aacd0c3c97af5aef653cb51da4f2708015a7f
Nick Robin <nrobin24@uw.edu>
2014-04-10 22:03:14 -0700

    Change icons and make only show on amazon.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,21 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
+// chrome.runtime.onInstalled.addListener(function (details) {
+//     console.log('previousVersion', details.previousVersion);
+// });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
-});
+function onWebNav(details) {
+    if (details.frameId === 0) {
+        // Top-level frame
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var filter = {
+    url: [{
+        hostEquals: 'www.amazon.com'
+    }]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
 
 console.log('\'Allo \'Allo! Event Page for Page Action');
