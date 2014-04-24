3f944f92083f2b219d721c1fa0b786da11b8ba51
Nick Robin <nick@trialomics.com>
2014-04-24 15:24:43 -0700

    Clean it up

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -6,20 +6,9 @@ var colorResultsContaining = function (string) {
   $("div[id*='result_']").has("*:contains('" + string + "')").css("background-color","red");
 }
 
-// $.getJSON('https://cert.firebaseio.com/certStrings/.json', function (data) {
-//   $.each(data.items, function (i, item) {
-//     console.log(item);
-//     //colorResultsContaining(thisCertString);
-//   });
-// });
-
 $.ajax( 'https://cert.firebaseio.com/certStrings/.json' )
   .done(function(data) {
     $.each(data, function (i, string) {
-      console.log('sup');
-      console.log(string);
       colorResultsContaining(string);
     });
   });
-
-
