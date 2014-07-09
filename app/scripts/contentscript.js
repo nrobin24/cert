e6dcd33002902a822d5d7da116774c1f4f7120d1
Nick Robin <nick@trialomics.com>
2014-07-09 09:36:44 -0700

    Fix bugs
    Previously some results were not being painted by cert after hitting
    the search button in amazon. If you then hit refresh in the browser the
    results did get painted. So, it seems like amazon uses asynchronous
    loading that was happening after the content script was already run.
    
    Now, the search loop is triggered by any changes to the dom, using
    jQuery to bind ‘DOMSubtreeModified’ on the body tag. This lead to
    search being called >100 times on each page load, so I added a rate
    limiter, which seemed to reduce it to ~10 searches per page load.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,14 +1,41 @@
+/* jshint quotmark: false */
 /* global $ */
+/* global _ */
 
 'use strict';
 
-var colorResultsContaining = function (string) {
-  $("div[id*='result_']").has("*:contains('" + string + "')").css("background-color","red");
-}
-
+// get the list of strings from a remote resource, load them locally so they can be used for searching the DOM of amazon results pages
+var searchStrings = [];
 $.ajax( 'https://cert.firebaseio.com/certStrings/.json' )
   .done(function(data) {
-    $.each(data, function (i, string) {
-      colorResultsContaining(string);
+    searchStrings = data;
+});
+
+// use jQuery to select divs that have an ID containing the string 'result_' which also have a search string
+// this is specifically designed to capture search results from amazon.com, amazon uses the 'result_' as an id prefix for the divs we are targetting
+var resultsContaining = function (string) {
+    return $("div[id*='result_']").has("*:contains('" + string + "')");
+};
+
+// search through the DOM, find nodes specified by calling resultsContaining
+var runSearch = function () {
+    console.log('searching');
+    var searchResults = [];
+    _.each(searchStrings, function (searchString) {
+        var hasResults = (resultsContaining(searchString).length > 0);
+        if (hasResults){
+            // log which search string yielded any results (one or many, doesn't count them any differently)
+            searchResults.push(searchString);
+            // paint the nodes containing the search string with a red background
+            resultsContaining(searchString).css("background-color","red");
+        }
     });
-  });
+    console.log(searchResults);
+};
+
+// since this can get called a lot of times really quickly, but only needs to run once a second or so, use a rate limiter
+// use underscore.js rate limiter
+var throttledSearch = _.throttle(runSearch, 1000);
+
+// listen for any changes to the DOM, every time a change is detected run the throttledSearch function
+$('body').bind('DOMSubtreeModified', throttledSearch);
